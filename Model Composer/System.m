419b043f61c8c4e7d8a5c9abfb4d271c395676e5
Fatih Serhat Yalçındere <fsydere99@gmail.com>
2025-07-02 12:47:32 +0300

    Threshold detector Done

--- Model Composer/System.m
+++ Model Composer/System.m
@@ -20,10 +20,13 @@ JustNoiseSamples = Fs*JustNoiseTime;
     'PW',PW, 'PRI',PRI, 'Fc',Fc, 'SNR_dB',SNR_dB, ...
     'CarrierAmplitude',CarrierAmplitude);
 
-before_mean_noise = zeros(1,length(SNR_dB));
+Before_mean_noise = zeros(1,length(SNR_dB));
+After_mean_noise = zeros(1,length(SNR_dB));
+
 
 for threshold_SNR_idx = 1:length(SNR_dB)
-    before_mean_noise(threshold_SNR_idx) = JustNoiseSamples * mean(abs(noisySignals(1:JustNoiseSamples,threshold_SNR_idx)).^2);
+    Before_mean_noise(threshold_SNR_idx) = JustNoiseSamples * mean(abs(noisySignals(1:JustNoiseSamples,threshold_SNR_idx)).^2);
+    After_mean_noise(threshold_SNR_idx) = mean(abs(noisySignals(1:JustNoiseSamples,threshold_SNR_idx)).^2);
 end
 
 [IFM_results] = IFM(noisySignals,'Fs',Fs, ...
